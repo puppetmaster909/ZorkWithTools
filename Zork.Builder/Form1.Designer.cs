dda84145dedc0b331d0796549a607fdcbc91bd2e
Nabil Sekirime <nabil@soulcadeinteractive.com>
2019-10-22 18:08:05 -0400

    Added Node Support

--- Zork.Builder/Form1.Designer.cs
+++ Zork.Builder/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.controlNodeEditor1 = new SampleCommon.ControlNodeEditor();
+            this.SuspendLayout();
+            // 
+            // controlNodeEditor1
+            // 
+            this.controlNodeEditor1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.controlNodeEditor1.BackgroundImage = global::Zork.Builder.Properties.Resources.node_background;
+            this.controlNodeEditor1.Location = new System.Drawing.Point(1, 2);
+            this.controlNodeEditor1.Name = "controlNodeEditor1";
+            this.controlNodeEditor1.Size = new System.Drawing.Size(796, 445);
+            this.controlNodeEditor1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.controlNodeEditor1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private SampleCommon.ControlNodeEditor controlNodeEditor1;
     }
 }
 
